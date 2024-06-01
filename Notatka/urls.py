5ab4de61646d022e80428d1b22efa3983eb87f2b
MrFajFel <127607303+MrFajFel@users.noreply.github.com>
2024-06-01 12:30:29 +0200

    Ostatnie poprawki

--- Notatka/urls.py
+++ Notatka/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('note/', include('aplikacja.urls', namespace='notatki'))
+    path('', include('aplikacja.urls', namespace='notatki'))
 ]
